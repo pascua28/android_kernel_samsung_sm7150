1122beef86c6165ea1fd370156d2021b47b620a6
Alexey Dobriyan <adobriyan@gmail.com>
2018-04-10 16:32:01 -0700

    proc: faster /proc/cmdline
    Use seq_puts() and skip format string processing.
    
    Link: http://lkml.kernel.org/r/20180309222948.GB3843@avx2
    Change-Id: I31d072820c37263499d00d08af5ee5f2881f92d5
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -16,7 +16,8 @@ static int cmdline_proc_show(struct seq_file *m, void *v)
 		return 0;
 	}
 #endif
-	seq_printf(m, "%s\n", saved_command_line);
+	seq_puts(m, saved_command_line);
+	seq_putc(m, '\n');
 	return 0;
 }
 
