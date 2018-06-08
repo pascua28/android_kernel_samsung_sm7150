19e2ea6e9b60f96fed15635225972b8e05b12c55
Matthew Wilcox <mawilcox@microsoft.com>
2018-06-07 17:10:45 -0700

    lib/idr.c: remove simple_ida_lock
    Improve the scalability of the IDA by using the per-IDA xa_lock rather
    than the global simple_ida_lock.  IDAs are not typically used in
    performance-sensitive locations, but since we have this lock anyway, we
    can use it.  It is also a step towards converting the IDA from the radix
    tree to the XArray.
    
    [akpm@linux-foundation.org: idr.c needs xarray.h]
    Link: http://lkml.kernel.org/r/20180331125332.GF13332@bombadil.infradead.org
    Signed-off-by: Matthew Wilcox <mawilcox@microsoft.com>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: Tejun Heo <tj@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Mr-Morat <mtarom124@gmail.com>
    Signed-off-by: manipvlator <mr.morat14@gmail.com>

--- lib/idr.c
+++ lib/idr.c
@@ -488,8 +488,11 @@ int ida_alloc_range(struct ida *ida, unsigned int min, unsigned int max,
 		max = INT_MAX;
 
 again:
+	if (!ida_pre_get(ida, gfp_mask))
+		return -ENOMEM;
+
 	xa_lock_irqsave(&ida->ida_rt, flags);
-	ret = ida_get_new_above(ida, min, &id);
+	ret = ida_get_new_above(ida, start, &id);
 	if (!ret) {
 		if (id > max) {
 			ida_remove(ida, id);
