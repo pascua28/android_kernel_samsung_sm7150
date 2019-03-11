8fd9373fa833cc6ce3ac5c2be0ffa6c278ad7bf1
Minchan Kim <minchan@google.com>
2019-03-11 11:37:42 +0900

    Revert "mm: add speculative page fault vmstats"
    This reverts commit 726581778c6072ce5e9b2210e3075d8b9034fbda.
    
    Bug: 128240262
    Change-Id: I3ba5c0ab739af15061dd376fcb15366e9644ed4f
    Signed-off-by: Minchan Kim <minchan@google.com>

--- mm/memory.c
+++ mm/memory.c
@@ -4683,7 +4683,6 @@ int __handle_speculative_fault(struct mm_struct *mm, unsigned long address,
 	 * If there is no need to retry, don't return the vma to the caller.
 	 */
 	if (ret != VM_FAULT_RETRY) {
-		count_vm_event(SPECULATIVE_PGFAULT);
 		put_vma(vmf.vma);
 		*vma = NULL;
 		check_sync_rss_stat(current);
