39e9a261a4dedac6e4fb0cd8e6c77c5c4efc601e
Jebaitedneko <Jebaitedneko@gmail.com>
2021-08-19 06:55:05 +0530

    qcacld-3.0: disable stats dump when !WLAN_DEBUG
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_wext.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_wext.c
@@ -3164,6 +3164,7 @@ static void hdd_display_stats_help(void)
 int hdd_wlan_dump_stats(struct hdd_adapter *adapter, int value)
 {
 	int ret = 0;
+#ifdef WLAN_DEBUG
 	QDF_STATUS status;
 	struct hdd_context *hdd_ctx = WLAN_HDD_GET_CTX(adapter);
 
@@ -3200,6 +3201,7 @@ int hdd_wlan_dump_stats(struct hdd_adapter *adapter, int value)
 		}
 		break;
 	}
+#endif
 	return ret;
 }
 
