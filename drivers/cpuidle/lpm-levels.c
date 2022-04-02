a50cc768ec1bb2b5431ceaad55b3fb1e2e3013e1
Alexander Winkowski <dereference23@outlook.com>
2022-04-02 14:31:12 +0000

    lpm-levels: Don't stop the tick if it's not beneficial
    There is no point in stopping the scheduler tick if the expected
    idle duration is shorter than the tick period length.
    
    Change-Id: I9b6a1c1952f2d45fcc628ca7e86e898bb95f799a
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -647,13 +647,11 @@ static inline bool is_cpu_biased(int cpu, uint64_t *bias_time)
 }
 
 static int cpu_power_select(struct cpuidle_device *dev,
-		struct lpm_cpu *cpu)
+		struct lpm_cpu *cpu, s64 sleep_us)
 {
 	int best_level = 0;
 	uint32_t latency_us = pm_qos_request_for_cpu(PM_QOS_CPU_DMA_LATENCY,
 							dev->cpu);
-	ktime_t delta_next;
-	s64 sleep_us = ktime_to_us(tick_nohz_get_sleep_length(&delta_next));
 	uint32_t modified_time_us = 0;
 	uint32_t next_event_us = 0;
 	int i, idx_restrict;
@@ -1390,11 +1388,15 @@ static int lpm_cpuidle_select(struct cpuidle_driver *drv,
 		struct cpuidle_device *dev, bool *stop_tick)
 {
 	struct lpm_cpu *cpu = per_cpu(cpu_lpm, dev->cpu);
+	ktime_t delta_next;
+	ktime_t duration = tick_nohz_get_sleep_length(&delta_next);
+	if (duration <= TICK_NSEC)
+		*stop_tick = false;
 
 	if (!cpu)
 		return 0;
 
-	return cpu_power_select(dev, cpu);
+	return cpu_power_select(dev, cpu, ktime_to_us(duration));
 }
 
 void update_ipi_history(int cpu)
