d1227ba33402d37773945d31d63eaba5e2832a28
Sultan Alsawaf <sultan@kerneltoast.com>
2022-04-20 00:53:28 -0700

    drm/msm: Eliminate unnecessary snprintf() usage from hot paths
    There's no reason to constantly use snprintf() to generate pretty debug
    strings from hot paths. We don't need them, so remove them.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_drm.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_drm.c
@@ -166,9 +166,7 @@ void dsi_convert_to_drm_mode(const struct dsi_display_mode *dsi_mode,
 			dsi_mode->timing.sot_hs_mode ? ( dsi_mode->timing.phs_mode ? "PHS" : "HS") : "NS");
 #else
 	/* set mode name */
-	snprintf(drm_mode->name, DRM_DISPLAY_MODE_LEN, "%dx%dx%dx%d",
-			drm_mode->hdisplay, drm_mode->vdisplay,
-			drm_mode->vrefresh, drm_mode->clock);
+	*drm_mode->name = '\0';
 #endif
 }
 
