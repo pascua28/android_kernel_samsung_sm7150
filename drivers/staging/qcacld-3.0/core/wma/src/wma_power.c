a2af0e5265852cc0eb5f1fc224f4975522d1b9fa
Gangadhar Kavalastramath <quic_gkavalas@quicinc.com>
2022-05-04 21:21:38 +0530

    qcacld-3.0: Initialize local struct objects
    Initialize struct cm_roam_values_copy to avoid using uninitialized
    parameters in wlan_cm_roam_cfg_set_value().
    Initialize struct pdev_params to zero to avoid using uninitialized
    parameters in wmi_unified_pdev_param_send().
    
    Change-Id: I820db09840431487f1756695a0562f8a794f549f
    CRs-Fixed: 3187194
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/staging/qcacld-3.0/core/wma/src/wma_power.c
+++ drivers/staging/qcacld-3.0/core/wma/src/wma_power.c
@@ -1207,7 +1207,7 @@ QDF_STATUS wma_process_tx_power_limits(WMA_HANDLE handle,
 	int32_t ret = 0;
 	uint32_t txpower_params2g = 0;
 	uint32_t txpower_params5g = 0;
-	struct pdev_params pdevparam;
+	struct pdev_params pdevparam = {};
 
 	if (!wma || !wma->wmi_handle) {
 		WMA_LOGE("%s: WMA is closed, can not issue tx power limit",
@@ -1567,7 +1567,7 @@ QDF_STATUS wma_set_idle_ps_config(void *wma_ptr, uint32_t idle_ps)
 {
 	int32_t ret;
 	tp_wma_handle wma = (tp_wma_handle) wma_ptr;
-	struct pdev_params pdevparam;
+	struct pdev_params pdevparam = {};
 
 	WMA_LOGD("WMA Set Idle Ps Config [1:set 0:clear] val %d", idle_ps);
 
