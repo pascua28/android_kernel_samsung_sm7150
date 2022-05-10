dac7451aaf66d2bae2f6d3204e410e5ea346b087
JJ Lee <leejj@google.com>
2022-05-10 07:26:04 +0000

    msm: adsprpc: fix bad array access with invalid channel id
    Valid cid in struct fastrpc_file ranges between 0 and NUM_CHANNELS.
    Return right after validating cid and sctx to avoid bad array access
    and NULL pointer access later.
    
    bug: 227264184
    Change-Id: Ib21ca296b397947aae5cf797b1665a5e230447bc
    Signed-off-by: JJ Lee <leejj@google.com>
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -1976,14 +1976,14 @@ static int fastrpc_internal_invoke(struct fastrpc_file *fl, uint32_t mode,
 	VERIFY(err, cid >= ADSP_DOMAIN_ID && cid < NUM_CHANNELS);
 	if (err) {
 		err = -ECHRNG;
-		goto bail;
+		return err;
 	}
 
 	VERIFY(err, fl->sctx != NULL);
 
 	if (err) {
 		err = -EBADR;
-		goto bail;
+		return err;
 	}
 
 	perf_counter = getperfcounter(fl, PERF_COUNT);
