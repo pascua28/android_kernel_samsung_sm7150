e18c45727d7dcff2a38a77b7f09a71d677a6c8cf
阿菌•未霜 <799620521@qq.com>
2022-08-02 00:23:53 +0800

    lib/lz4: Import arm64 V8 ASM lz4 decompression acceleration
    Change-Id: I3c8dd91df090bb692784a6b7a61c8877b1e1dfba
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: pix106 <sbordenave@gmail.com>
    (cherry picked from commit d702746cbde3e99adfb1483a6f4d9ef404ebe590)

--- include/linux/lz4.h
+++ include/linux/lz4.h
@@ -14,4 +14,8 @@
 #define LZ4HC_DEFAULT_CLEVEL	LZ4HC_CLEVEL_DEFAULT
 #define LZ4HC_MAX_CLEVEL	LZ4HC_CLEVEL_MAX
 
+ssize_t LZ4_arm64_decompress_safe(const void *source, void *dest, size_t inputSize, size_t outputSize, bool dip);
+
+ssize_t LZ4_arm64_decompress_safe_partial(const void *source, void *dest, size_t inputSize, size_t outputSize, bool dip);
+
 #endif
