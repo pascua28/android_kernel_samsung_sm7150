3b65dfb247dfc143cfc4ae199ac62ce01cd33bad
Samuel Pascua <pascua.samuel.14@gmail.com>
2023-05-13 21:07:43 +0800

    vmlinux: keep pgo sections
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- include/asm-generic/vmlinux.lds.h
+++ include/asm-generic/vmlinux.lds.h
@@ -223,27 +223,27 @@
 	__llvm_prf_data : AT(ADDR(__llvm_prf_data) - LOAD_OFFSET) {	\
 		__llvm_prf_start = .;					\
 		__llvm_prf_data_start = .;				\
-		*(__llvm_prf_data)					\
+		KEEP(*(__llvm_prf_data))				\
 		__llvm_prf_data_end = .;				\
 	}								\
 	__llvm_prf_cnts : AT(ADDR(__llvm_prf_cnts) - LOAD_OFFSET) {	\
 		__llvm_prf_cnts_start = .;				\
-		*(__llvm_prf_cnts)					\
+		KEEP(*(__llvm_prf_cnts))				\
 		__llvm_prf_cnts_end = .;				\
 	}								\
 	__llvm_prf_names : AT(ADDR(__llvm_prf_names) - LOAD_OFFSET) {	\
 		__llvm_prf_names_start = .;				\
-		*(__llvm_prf_names)					\
+		KEEP(*(__llvm_prf_names))				\
 		__llvm_prf_names_end = .;				\
 	}								\
 	__llvm_prf_vals : AT(ADDR(__llvm_prf_vals) - LOAD_OFFSET) {	\
 		__llvm_prf_vals_start = .;				\
-		*(__llvm_prf_vals)					\
+		KEEP(*(__llvm_prf_vals))				\
 		__llvm_prf_vals_end = .;				\
 	}								\
 	__llvm_prf_vnds : AT(ADDR(__llvm_prf_vnds) - LOAD_OFFSET) {	\
 		__llvm_prf_vnds_start = .;				\
-		*(__llvm_prf_vnds)					\
+		KEEP(*(__llvm_prf_vnds))				\
 		__llvm_prf_vnds_end = .;				\
 		__llvm_prf_end = .;					\
 	}
