9f821f5ce28ff440bc69769bf1fdc377445d6586
Diaz1401 <reagor8161@outlook.com>
2023-05-26 22:02:18 +0700

    drivers/platform: ipa: fix enum/integer mismatch warnings
    ../drivers/platform/msm/ipa/ipa_api.c:3743:6: warning: conflicting types for 'ipa_register_client_callback' due to enum/integer mismatch; have 'void(int (*)(bool), bool (*)(void), enum ipa_client_type)' {aka 'void(int (*)(_Bool), _Bool (*)(void), enum ipa_client_type)'} [-Wenum-int-mismatch]
     3743 | void ipa_register_client_callback(int (*client_cb)(bool is_lock),
          |      ^~~~~~~~~~~~~~~~~~~~~~~~~~~~
    In file included from ../drivers/platform/msm/ipa/ipa_api.h:9,
                     from ../drivers/platform/msm/ipa/ipa_api.c:16:
    ../drivers/platform/msm/ipa/ipa_common_i.h:491:6: note: previous declaration of 'ipa_register_client_callback' with type 'void(int (*)(bool), bool (*)(void), u32)' {aka 'void(int (*)(_Bool), _Bool (*)(void), unsigned int)'}
      491 | void ipa_register_client_callback(int (*client_cb)(bool is_lock),
          |      ^~~~~~~~~~~~~~~~~~~~~~~~~~~~
    ../drivers/platform/msm/ipa/ipa_api.c:3751:6: warning: conflicting types for 'ipa_deregister_client_callback' due to enum/integer mismatch; have 'void(enum ipa_client_type)' [-Wenum-int-mismatch]
     3751 | void ipa_deregister_client_callback(enum ipa_client_type client)
          |      ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    ../drivers/platform/msm/ipa/ipa_common_i.h:494:6: note: previous declaration of 'ipa_deregister_client_callback' with type 'void(u32)' {aka 'void(unsigned int)'}
      494 | void ipa_deregister_client_callback(u32 ipa_ep_idx);
          |      ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    Signed-off-by: Diaz1401 <reagor8161@outlook.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>
    Signed-off-by: farizmaul <nickshade88@gmail.com>

--- drivers/platform/msm/ipa/ipa_common_i.h
+++ drivers/platform/msm/ipa/ipa_common_i.h
@@ -487,7 +487,7 @@ int ipa_wigig_send_msg(int msg_type,
 	enum ipa_client_type client, bool to_wigig);
 
 void ipa_register_client_callback(int (*client_cb)(bool is_lock),
-			bool (*teth_port_state)(void), u32 ipa_ep_idx);
+			bool (*teth_port_state)(void), enum ipa_client_type ipa_ep_idx);
 
 void ipa_deregister_client_callback(enum ipa_client_type ipa_ep_idx);
 
