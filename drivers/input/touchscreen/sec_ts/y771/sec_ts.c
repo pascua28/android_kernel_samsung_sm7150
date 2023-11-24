292be55411572b077d2bcd9becb2de96a0e355ca
LuK1337 <priv.luk@gmail.com>
2023-11-25 00:29:27 +0100

    input: sec_ts: Use KEY_WAKEUP for DT2W
    Change-Id: I5b069a496b9c823f77220482172fd0a0bfd57436

--- drivers/input/touchscreen/sec_ts/y771/sec_ts.c
+++ drivers/input/touchscreen/sec_ts/y771/sec_ts.c
@@ -1579,7 +1579,9 @@ static void sec_ts_read_event(struct sec_ts_data *ts)
 					if (p_gesture_status->gesture_id == SEC_GESTURE_ID_FOD_LONG || p_gesture_status->gesture_id == SEC_GESTURE_ID_FOD_NORMAL) {
 						ts->scrub_id = SPONGE_EVENT_TYPE_FOD;
 						input_info(true, &ts->client->dev, "%s: FOD: %s\n", __func__, p_gesture_status->gesture_id ? "normal" : "long");
-						input_report_key(ts->input_dev, KEY_BLACK_UI_GESTURE, 1);
+					input_report_key(ts->input_dev, KEY_WAKEUP, 1);
+					input_sync(ts->input_dev);
+					input_report_key(ts->input_dev, KEY_WAKEUP, 0);
 					} else if (p_gesture_status->gesture_id == SEC_GESTURE_ID_FOD_RELEASE) {
 						ts->scrub_id = SPONGE_EVENT_TYPE_FOD_RELEASE;
 						input_info(true, &ts->client->dev, "%s: FOD release\n", __func__);
