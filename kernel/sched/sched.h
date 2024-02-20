937f1243fe6f467f0bf0dc195c463e7c77d1e848
Samuel Pascua <pascua.samuel.14@gmail.com>
2024-02-20 16:13:59 +0800

    Revert "BACKPORT: sched: promote nodes out of CONFIG_SCHED_DEBUG"
    This reverts commit 13d9848a9bb11765abce511ec19335f5d8626f2e.

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1469,7 +1469,15 @@ static inline void __set_task_cpu(struct task_struct *p, unsigned int cpu)
 #endif
 }
 
-#define const_debug __read_mostly
+/*
+ * Tunables that become constants when CONFIG_SCHED_DEBUG is off:
+ */
+#ifdef CONFIG_SCHED_DEBUG
+# include <linux/static_key.h>
+# define const_debug __read_mostly
+#else
+# define const_debug const
+#endif
 
 #define sched_feat(x) SCHED_FEAT_##x
 
