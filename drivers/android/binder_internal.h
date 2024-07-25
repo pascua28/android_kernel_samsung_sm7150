6fe2f8200c58f24bc8b9042e48b89041df8bf859
Carlos Llamas <cmllamas@google.com>
2024-07-25 18:13:35 +0000

    BACKPORT: ANDROID: binder: fix KMI-break due to proc->dmap
    Add 'struct binder_proc_wrap' to support the addition of new members in
    'struct binder_proc' without breaking the KMI. In this case, proc->dmap
    was backported from upstream and needs to be migrated into this wrapper.
    
    Avoids the following KMI issue:
    
      function symbol 'int __traceiter_binder_transaction_received(void*, struct binder_transaction*)' changed
        CRC changed from 0x74e9c98b to 0x7af6cf5a
    
      type 'struct binder_proc' changed
        byte size changed from 584 to 600
        member 'struct dbitmap dmap' was added
        16 members ('struct list_head todo' .. 'u64 android_oem_data1') changed
          offset changed by 128
    
    Pzqqt: Backport to android12-5.10
    
    Bug: 298520209
    Change-Id: Icbbee14a8f16d0881faf8d5673582e785f98e8cf
    Signed-off-by: Carlos Llamas <cmllamas@google.com>

--- drivers/android/binder_internal.h
+++ drivers/android/binder_internal.h
@@ -405,8 +405,6 @@ struct binder_priority {
  * @freeze_wait:          waitqueue of processes waiting for all outstanding
  *                        transactions to be processed
  *                        (protected by @inner_lock)
- * @dmap                  dbitmap to manage available reference descriptors
- *                        (protected by @outer_lock)
  * @todo:                 list of work for this process
  *                        (protected by @inner_lock)
  * @stats:                per-process binder statistics
@@ -455,7 +453,6 @@ struct binder_proc {
 	bool sync_recv;
 	bool async_recv;
 	wait_queue_head_t freeze_wait;
-	struct dbitmap dmap;
 	struct list_head todo;
 #ifdef CONFIG_ANDROID_BINDER_LOGS
 	struct binder_stats stats;
@@ -484,16 +481,21 @@ struct binder_proc {
  * @delivered_freeze:     list of delivered freeze notification
  *                        (protected by @inner_lock)
  * @lock:            protects binder_proc->alloc fields
+ * @dmap             dbitmap to manage available reference descriptors
+ *                   (protected by @proc.outer_lock)
  *
  * Extended binder_proc -- needed to add the "cred" field without
  * changing the KMI for binder_proc.
  * Extended binder_proc -- needed to add the "lock" field without
  * changing the KMI for binder_proc->alloc.
+ * Extended binder_proc -- needed to add the "dmap" field without
+ * changing the KMI for binder_proc.
  */
 struct binder_proc_ext {
 	struct binder_proc proc;
 	const struct cred *cred;
 	spinlock_t lock;
+	struct dbitmap dmap;
 	struct list_head delivered_freeze;
 };
 
