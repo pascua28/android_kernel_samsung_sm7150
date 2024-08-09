dab0f7d347ec05b8e48caa611058038017383723
Samuel Pascua <pascua.samuel.14@gmail.com>
2024-08-09 15:21:55 +0800

    android: rom_notifier: micro-optimize
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- include/linux/rom_notifier.h
+++ include/linux/rom_notifier.h
@@ -3,6 +3,7 @@
  * Copyright (C) 2024 Samuel Pascua <pascua.samuel.14@gmail.com>.
  */
 
+#include <linux/cache.h>
 #include <linux/types.h>
 
-extern bool is_aosp;
+extern bool is_aosp __read_mostly;
