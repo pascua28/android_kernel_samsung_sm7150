d67845e35d5c075c57fa300377e6532e8ad101dd
Felix Fietkau <nbd@nbd.name>
2024-11-20 14:59:58 +0800

    treewide: strip procfs
    Strip non-essential /proc functionality to reduce code size
    
    Imported from openwrt

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -1958,10 +1958,12 @@ void __init init_mm_internals(void)
 	start_shepherd_timer();
 #endif
 #ifdef CONFIG_PROC_FS
-	proc_create("buddyinfo", 0444, NULL, &buddyinfo_file_operations);
-	proc_create("pagetypeinfo", 0400, NULL, &pagetypeinfo_file_operations);
+	if (!IS_ENABLED(CONFIG_PROC_STRIPPED)) {
+		proc_create("buddyinfo", 0444, NULL, &buddyinfo_file_operations);
+		proc_create("pagetypeinfo", 0400, NULL, &pagetypeinfo_file_operations);
+		proc_create("zoneinfo", 0444, NULL, &zoneinfo_file_operations);
+	}
 	proc_create("vmstat", 0444, NULL, &vmstat_file_operations);
-	proc_create("zoneinfo", 0444, NULL, &zoneinfo_file_operations);
 #endif
 }
 
