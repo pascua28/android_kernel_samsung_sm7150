72e9552eb8edd59ace6bc95df47bb36db67f2413
Samuel Pascua <pascua.samuel.14@gmail.com>
2024-11-22 10:45:44 +0800

    execprog: add post-fs-data stage
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- scripts/prime.sh
+++ scripts/prime.sh
@@ -1,4 +1,7 @@
 #! /vendor/bin/sh
 
+# Commands after this line will be executed on post-fs-data
+while [ "$(getprop vold.post_fs_data_done)" != 1 ]; do sleep 0.25s; done
+
 # Re-enable SELinux
 echo "97" > /sys/fs/selinux/enforce
