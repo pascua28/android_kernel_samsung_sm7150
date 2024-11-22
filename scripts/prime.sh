032f3a7de38b01a42d77e53fe52f76803c8c8c7f
Samuel Pascua <pascua.samuel.14@gmail.com>
2024-11-22 10:51:08 +0800

    execprog: enable fuse passthrough via build.prop
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- scripts/prime.sh
+++ scripts/prime.sh
@@ -1,5 +1,17 @@
 #! /vendor/bin/sh
 
+# Run under a new tmpfs to avoid /dev selabel
+mkdir /dev/ep
+mount -t tmpfs nodev /dev/ep
+
+cat /system/build.prop > /dev/ep/build.prop
+echo "
+persist.sys.fuse.passthrough.enable=true
+" >> /dev/ep/build.prop
+chmod 600 /dev/ep/build.prop
+mount --bind /dev/ep/build.prop /system/build.prop
+chcon "u:object_r:system_file:s0" /system/build.prop
+
 # Commands after this line will be executed on post-fs-data
 while [ "$(getprop vold.post_fs_data_done)" != 1 ]; do sleep 0.25s; done
 
