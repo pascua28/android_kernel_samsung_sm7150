310af1ee8d660a9d95341a27103ab835fc33ff60
eun0115 <gianpaoloestacio5@gmail.com>
2024-12-19 08:17:32 +0000

    init: Fix section mismatch
    WARNING: vmlinux.o(.text+0xb68): Section mismatch in reference from the function mount_sar_ramdisk() to the function .init.text:clean_rootfs()
    The function mount_sar_ramdisk() references
    the function __init clean_rootfs().
    This is often because mount_sar_ramdisk lacks a __init
    annotation or the annotation of clean_rootfs is wrong.
    
    WARNING: vmlinux.o(.text+0xb74): Section mismatch in reference from the function mount_sar_ramdisk() to the function .init.text:unpack_to_rootfs()
    The function mount_sar_ramdisk() references
    the function __init unpack_to_rootfs().
    This is often because mount_sar_ramdisk lacks a __init
    annotation or the annotation of unpack_to_rootfs is wrong.
    
    WARNING: vmlinux.o(.text+0xb80): Section mismatch in reference from the function mount_sar_ramdisk() to the function .init.text:load_default_modules()
    The function mount_sar_ramdisk() references
    the function __init load_default_modules().
    This is often because mount_sar_ramdisk lacks a __init
    annotation or the annotation of load_default_modules is wrong.
    
    Signed-off-by: eun0115 <gianpaoloestacio5@gmail.com>
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- init/do_mounts.c
+++ init/do_mounts.c
@@ -55,7 +55,7 @@ dev_t ROOT_DEV;
 #ifdef CONFIG_ANDROID_SAR_RAMDISK
 static int __initdata android_bootmode;
 
-extern int mount_sar_ramdisk(char*);
+extern int __init mount_sar_ramdisk(char*);
 
 static int __init android_bootmode_setup(char *str) {
 	android_bootmode = simple_strtol(str,NULL,0);
