e2f36a4f8f53992e07d6160802edffaf94d059a0
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2025-02-06 00:03:48 +0600

    qcacmn: i_qdf_trace: Align __qdf_bug() declaration with WLAN_DEBUG dependency
    The __qdf_bug() implementation in qdf_trace.c is conditionally compiled
    based on WLAN_DEBUG, PANIC_ON_BUG, and CONFIG_SLAB_DEBUG. However, the
    corresponding declaration in i_qdf_trace.h lacks the WLAN_DEBUG
    dependency, leading to undefined symbol errors during compilation when
    WLAN_DEBUG is not set.
    
    This fixes the following compilation error when building with
    CONFIG_KASAN=y:
    
    ld.lld: error: undefined symbol: __qdf_bug
    >>> referenced by target_if_direct_buf_rx_main.c
    >>>               staging/qca-wifi-host-cmn/target_if/direct_buf_rx/src/target_if_direct_buf_rx_main.o:(target_if_dbr_replenish_ring) in archive drivers/built-in.a
    >>> referenced by target_if_direct_buf_rx_main.c
    >>>               staging/qca-wifi-host-cmn/target_if/direct_buf_rx/src/target_if_direct_buf_rx_main.o:(target_if_dbr_replenish_ring) in archive drivers/built-in.a
    >>> referenced by wma_features.c
    >>>               staging/qcacld-3.0/core/wma/src/wma_features.o:(wma_get_rx_retry_cnt) in archive drivers/built-in.a
    >>> referenced 1224 more times
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/staging/qca-wifi-host-cmn/qdf/linux/src/i_qdf_trace.h
+++ drivers/staging/qca-wifi-host-cmn/qdf/linux/src/i_qdf_trace.h
@@ -338,8 +338,7 @@ static inline void qdf_vprint(const char *fmt, va_list args)
 }
 #endif
 
-#ifdef PANIC_ON_BUG
-#ifdef CONFIG_SLUB_DEBUG
+#if defined(WLAN_DEBUG) && defined(PANIC_ON_BUG) && defined(CONFIG_SLUB_DEBUG)
 /**
  * __qdf_bug() - Calls BUG() when the PANIC_ON_BUG compilation option is enabled
  *
@@ -372,13 +371,14 @@ static inline void qdf_vprint(const char *fmt, va_list args)
  * Return: None
  */
 void __qdf_bug(void);
-#else /* CONFIG_SLUB_DEBUG */
+#else
 static inline void __qdf_bug(void)
 {
 	BUG();
 }
-#endif /* CONFIG_SLUB_DEBUG */
+#endif
 
+#ifdef PANIC_ON_BUG
 /**
  * QDF_DEBUG_PANIC() - In debug builds, panic, otherwise do nothing
  * @reason_fmt: a format string containing the reason for the panic
