c0088cfa6bacb0aa6e5204aa7b6de0ad418f446a
Samuel Pascua <pascua.samuel.14@gmail.com>
2025-02-07 21:02:17 +0800

    Revert "fs: namespace preemption fix"
    This reverts commit 41d89480b4e0f039347b58ec0ab43e16833e599c.

--- fs/namespace.c
+++ fs/namespace.c
@@ -715,14 +715,11 @@ int __mnt_want_write(struct vfsmount *m)
 	 */
 	smp_mb();
 #ifdef CONFIG_RKP_NS_PROT
-	while (ACCESS_ONCE(mnt->mnt->mnt_flags) & MNT_WRITE_HOLD) {
+	while (ACCESS_ONCE(mnt->mnt->mnt_flags) & MNT_WRITE_HOLD)
 #else
-	while (READ_ONCE(mnt->mnt.mnt_flags) & MNT_WRITE_HOLD) {
+	while (ACCESS_ONCE(mnt->mnt.mnt_flags) & MNT_WRITE_HOLD)
 #endif
-		preempt_enable();
 		cpu_relax();
-		preempt_disable();
-	}
 	/*
 	 * After the slowpath clears MNT_WRITE_HOLD, mnt_is_readonly will
 	 * be set to match its requirements. So we must not load that until
