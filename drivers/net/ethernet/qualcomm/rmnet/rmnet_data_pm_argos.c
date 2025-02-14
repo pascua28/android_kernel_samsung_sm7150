8c5257472541cfb18bdb80f315d6afbbb893be6a
Samuel Pascua <pascua.samuel.14@gmail.com>
2025-02-14 08:50:35 +0800

    net: rmnet: always build argos booster
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- drivers/net/ethernet/qualcomm/rmnet/rmnet_data_pm_argos.c
+++ drivers/net/ethernet/qualcomm/rmnet/rmnet_data_pm_argos.c
@@ -19,6 +19,7 @@
 #include <linux/netdevice.h>
 #include <linux/if_arp.h>
 #include <linux/pm_qos.h>
+#include <linux/suspend.h>
 
 #include "rmnet_data_pm_argos.h"
 
@@ -360,7 +361,11 @@ static int rmnet_data_dev_cb(struct notifier_block *nb,
 		/* Set initial value */
 		rmnet_data_pm_argos_cb(NULL, 0, NULL);
 
+#ifdef CONFIG_ARGOS
 		ret = sec_argos_register_notifier(&rmnet_data_argos_nb, MIF_ARGOS_IPC_LABEL);
+#else
+		ret = register_pm_notifier(&rmnet_data_argos_nb);
+#endif
 		if (ret)
 			pr_err("Fail to register rmnet_data pm argos notifier block\n");
 		break;
@@ -369,7 +374,11 @@ static int rmnet_data_dev_cb(struct notifier_block *nb,
 			break;
 
 		pr_info("Reset rmnet_data_pm_argos configure\n");
+#ifdef CONFIG_ARGOS
 		ret = sec_argos_unregister_notifier(&rmnet_data_argos_nb, MIF_ARGOS_IPC_LABEL);
+#else
+		ret = unregister_pm_notifier(&rmnet_data_argos_nb);
+#endif
 		if (ret)
 			pr_err("Fail to unregister rmnet_data pm argos notifier block\n");
 
--- drivers/platform/msm/ipa/ipa_v3/ipa.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa.c
@@ -9319,7 +9319,6 @@ module_param(emulation_type, uint, 0000);
 MODULE_PARM_DESC(
 	emulation_type,
 	"emulation_type=N N can be 13 for IPA 3.5.1, 14 for IPA 4.0, 17 for IPA 4.5");
-#if defined(CONFIG_ARGOS)
 void ipa3_set_napi_chained_rx(bool enable)
 {
 	if (!ipa3_ctx)
@@ -9331,4 +9330,3 @@ void ipa3_set_napi_chained_rx(bool enable)
 		ipa3_ctx->enable_napi_chain = 0;
 }
 EXPORT_SYMBOL(ipa3_set_napi_chained_rx);
-#endif
