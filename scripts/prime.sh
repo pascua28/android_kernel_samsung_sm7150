bdf247840a22bde57c021b1812065e29abebeafe
Samuel Pascua <pascua.samuel.14@gmail.com>
2025-04-01 22:19:08 +0800

    scripts/prime.sh: set fuse passthrough enable using resetprop
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- scripts/prime.sh
+++ scripts/prime.sh
@@ -4,13 +4,7 @@
 mkdir /dev/ep
 mount -t tmpfs nodev /dev/ep
 
-cat /system/build.prop > /dev/ep/build.prop
-echo "
-persist.sys.fuse.passthrough.enable=true
-" >> /dev/ep/build.prop
-chmod 600 /dev/ep/build.prop
-mount --bind /dev/ep/build.prop /system/build.prop
-chcon "u:object_r:system_file:s0" /system/build.prop
+/dev/resetprop persist.sys.fuse.passthrough.enable true
 
 # Commands after this line will be executed on post-fs-data
 while [ "$(getprop vold.post_fs_data_done)" != 1 ]; do sleep 0.25s; done
