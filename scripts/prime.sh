24d10b4f264d349e6360a994e06dd29c24c20d4a
Samuel Pascua <pascua.samuel.14@gmail.com>
2025-04-03 09:18:58 +0800

    scripts/prime.sh: use qcom post_boot script
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- scripts/prime.sh
+++ scripts/prime.sh
@@ -4,10 +4,15 @@
 mkdir /dev/ep
 mount -t tmpfs nodev /dev/ep
 
-/dev/resetprop persist.sys.fuse.passthrough.enable true
+cat /vendor/bin/init.qcom.post_boot.sh > /dev/ep/init.qcom.post_boot.sh
 
-# Commands after this line will be executed on post-fs-data
-while [ "$(getprop vold.post_fs_data_done)" != 1 ]; do sleep 0.25s; done
+echo '
+/dev/resetprop persist.sys.fuse.passthrough.enable true
 
 # Re-enable SELinux
 echo "97" > /sys/fs/selinux/enforce
+' >> /dev/ep/init.qcom.post_boot.sh
+
+mount --bind /dev/ep/init.qcom.post_boot.sh /vendor/bin/init.qcom.post_boot.sh
+chmod 755 /vendor/bin/init.qcom.post_boot.sh
+chcon "u:object_r:vendor_qti_init_shell_exec:s0" /vendor/bin/init.qcom.post_boot.sh
