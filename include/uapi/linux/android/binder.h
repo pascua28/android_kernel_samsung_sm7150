b7768e9441ce2babc17cf07ba6f29d7ba639aada
Samuel Pascua <pascua.samuel.14@gmail.com>
2025-04-10 14:36:05 +0800

    android: binder: remove ioctl only used for debugging
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- include/uapi/linux/android/binder.h
+++ include/uapi/linux/android/binder.h
@@ -295,10 +295,9 @@ struct binder_frozen_status_info {
 #define BINDER_GET_NODE_DEBUG_INFO	_IOWR('b', 11, struct binder_node_debug_info)
 #define BINDER_GET_NODE_INFO_FOR_REF	_IOWR('b', 12, struct binder_node_info_for_ref)
 #define BINDER_SET_CONTEXT_MGR_EXT	_IOW('b', 13, struct flat_binder_object)
-#define BINDER_SET_SYSTEM_SERVER_PID		_IOW('b', 14, __u32)
-#define BINDER_FREEZE			_IOW('b', 15, struct binder_freeze_info)
-#define BINDER_GET_FROZEN_INFO		_IOWR('b', 16, struct binder_frozen_status_info)
-#define BINDER_ENABLE_ONEWAY_SPAM_DETECTION	_IOW('b', 17, __u32)
+#define BINDER_FREEZE			_IOW('b', 14, struct binder_freeze_info)
+#define BINDER_GET_FROZEN_INFO		_IOWR('b', 15, struct binder_frozen_status_info)
+#define BINDER_ENABLE_ONEWAY_SPAM_DETECTION	_IOW('b', 16, __u32)
 
 /*
  * NOTE: Two special error codes you should check for when calling
