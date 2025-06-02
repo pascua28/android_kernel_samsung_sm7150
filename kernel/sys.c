ec6c9215dac05692f2aff7c6ea8dfa4113c6c167
Samuel Pascua <pascua.samuel.14@gmail.com>
2025-06-02 09:11:02 +0800

    init: make eBPF spoofing optional
    cmdline will be patched during installation. Needed to unbreak support
    for Android 13.
    
    Change-Id: I445fcfc5066b932fe5c3edf98bfa041ae8cef12c
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1182,6 +1182,8 @@ static int override_release(char __user *release, size_t len)
 	return ret;
 }
 
+extern bool legacy_ebpf __read_mostly;
+
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
@@ -1193,7 +1195,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	    !strncmp(current->comm, "netbpfload", 10) ||
 	    !strncmp(current->comm, "netd", 4) ||
 	    !strncmp(current->comm, "uprobestats", 11)) {
-		if (current_uid().val == 0) {
+		if (current_uid().val == 0 && !legacy_ebpf) {
 #if defined(CONFIG_FAKE_UNAME_5_4)
 			strcpy(tmp.release, "5.4.200");
 #elif defined(CONFIG_FAKE_UNAME_5_10)
