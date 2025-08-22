060c0fe2bb91b3c43c27856c5afd4e8828ae0395
Sultanxda <sultanxda@gmail.com>
2025-08-22 14:06:08 +0300

    firmware: qcom: Remove debugfs dependency from tz-log driver
    Without this driver (specifically, its probe routines), the device crashes
    frequently, so it is needed even when debugfs is disabled.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/firmware/qcom/tz_log.c
+++ drivers/firmware/qcom/tz_log.c
@@ -333,7 +333,7 @@ static bool restore_from_hibernation;
 /*
  * Debugfs data structure and functions
  */
-
+#ifdef CONFIG_DEBUG_FS
 static int _disp_tz_general_stats(void)
 {
 	int len = 0;
@@ -878,7 +878,7 @@ const struct file_operations tzdbg_fops = {
 	.read    = tzdbgfs_read,
 	.open    = tzdbgfs_open,
 };
-
+#endif
 
 /*
  * Allocates log buffer from ION, registers the buffer at TZ
@@ -942,6 +942,7 @@ err:
 static int  tzdbgfs_init(struct platform_device *pdev)
 {
 	int rc = 0;
+#ifdef CONFIG_DEBUG_FS
 	int i;
 	struct dentry           *dent_dir;
 	struct dentry           *dent;
@@ -971,17 +972,19 @@ static int  tzdbgfs_init(struct platform_device *pdev)
 	return 0;
 err:
 	debugfs_remove_recursive(dent_dir);
-
+#endif
 	return rc;
 }
 
 static void tzdbgfs_exit(struct platform_device *pdev)
 {
+#ifdef CONFIG_DEBUG_FS
 	struct dentry           *dent_dir;
 
 	kzfree(tzdbg.disp_buf);
 	dent_dir = platform_get_drvdata(pdev);
 	debugfs_remove_recursive(dent_dir);
+#endif
 	if (g_qsee_log)
 		dma_free_coherent(&pdev->dev, QSEE_LOG_BUF_SIZE,
 					 (void *)g_qsee_log, coh_pmem);
