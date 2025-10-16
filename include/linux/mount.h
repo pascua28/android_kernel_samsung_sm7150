25f5b9d80964044fe717aa7eec661127638993d2
Samuel Pascua <pascua.samuel.14@gmail.com>
2025-10-16 17:39:36 +0800

    Revert "fs: import latest susfs4ksu"
    This reverts commit 09f82fbefe846f1598c4882a41dc8bb99a484c7e.
    
    Change-Id: Icb817dc1ca76d953bdb25376b97ca5fe7f836933

--- include/linux/mount.h
+++ include/linux/mount.h
@@ -72,9 +72,6 @@ struct vfsmount {
 #endif
 	int mnt_flags;
 	void *data;
-#ifdef CONFIG_KSU_SUSFS
-	u64 susfs_mnt_id_backup;
-#endif
 } __randomize_layout;
 
 struct file; /* forward dec */
--- kernel/sys.c
+++ kernel/sys.c
@@ -1197,9 +1197,6 @@ static int override_release(char __user *release, size_t len)
 }
 
 extern bool legacy_ebpf __read_mostly;
-#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-extern void susfs_spoof_uname(struct new_utsname* tmp);
-#endif
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
@@ -1216,9 +1213,6 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 				 current->comm, current->pid, tmp.release);
 		}
 	}
-#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-	susfs_spoof_uname(&tmp);
-#endif
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
