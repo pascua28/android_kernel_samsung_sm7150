b60eb476355ef69859a420616e99df91527bcc00
Samuel Pascua <pascua.samuel.14@gmail.com>
2025-11-10 20:38:21 +0800

    kernelsu: re-add support for ksu-next and rksu
    Change-Id: Icfb56b26a45f874f6207ac60c90e99df88de18a4
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- drivers/kernelsu/apk_sign.c
+++ drivers/kernelsu/apk_sign.c
@@ -355,5 +355,7 @@ bool is_manager_apk(char *path)
 	|| check_v2_signature(path, EXPECTED_SIZE, EXPECTED_HASH)  // ksu official
 	|| check_v2_signature(path, 384, "7e0c6d7278a3bb8e364e0fcba95afaf3666cf5ff3c245a3b63c8833bd0445cc4")  // 5ec1cff/KernelSU
 	|| check_v2_signature(path, 0x375, "484fcba6e6c43b1fb09700633bf2fb4758f13cb0b2f4457b80d075084b26c588")  // KOWX712/KernelSU
+	|| check_v2_signature(path, 0x363, "4359c171f32543394cbc23ef908c4bb94cad7c8087002ba164c8230948c21549")  // KernelSU-Next
+	|| check_v2_signature(path, 0x396, "f415f4ed9435427e1fdf7f1fccd4dbc07b3d6b8751e4dbcec6f19671f427870b")  // rksu
 	);
 }
