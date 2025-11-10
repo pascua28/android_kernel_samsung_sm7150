83542f82bf576bedb2219f34d860b4de737bf2e6
Samuel Pascua <pascua.samuel.14@gmail.com>
2025-11-10 20:26:51 +0800

    Revert "kernelsu: implement susfs v1.5.9"
    This reverts commit 7966ebb344696255d7d40bca5344336010471146.

--- drivers/kernelsu/ksu.c
+++ drivers/kernelsu/ksu.c
@@ -11,10 +11,6 @@
 #include "ksu.h"
 #include "throne_tracker.h"
 
-#ifdef CONFIG_KSU_SUSFS
-#include <linux/susfs.h>
-#endif
-
 static struct workqueue_struct *ksu_workqueue;
 
 bool ksu_queue_work(struct work_struct *work)
@@ -51,10 +47,6 @@ int __init kernelsu_init(void)
 	pr_alert("**                                                         **");
 	pr_alert("**     NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE    **");
 	pr_alert("*************************************************************");
-#endif
-
-#ifdef CONFIG_KSU_SUSFS
-	susfs_init();
 #endif
 
 	ksu_core_init();
