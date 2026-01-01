fd12fa7ea04b9df109e13db05108a22b070f5d9c
sidex15 <24408329+sidex15@users.noreply.github.com>
2026-01-01 22:09:51 +0800

    fs: kernel: susfs: remove inline hooks
    Since we're just going for manual hooks anyways (especially non-gki) just use the manual hooks instead...
    
    You have to use modified patch of susfs in the KernelSU/KernelSU-Next side:
    https://github.com/sidex15/KernelSU-Next/commit/0ee20c57b099af19f6381d6b9fb714e755dd6ee6
    
    Change-Id: If8601f0ed7e02e6b475f47eaf633e1101af18c86

--- fs/exec.c
+++ fs/exec.c
@@ -69,10 +69,6 @@
 
 #include <trace/events/task.h>
 
-#ifdef CONFIG_KSU_SUSFS
-#include <linux/susfs_def.h>
-#endif
-
 #include "internal.h"
 
 #include <trace/events/sched.h>
@@ -1716,17 +1712,6 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
-#ifdef CONFIG_KSU_SUSFS
-extern bool ksu_execveat_hook __read_mostly;
-extern bool ksu_su_compat_enabled __read_mostly;
-extern bool susfs_is_boot_completed_triggered __read_mostly;
-extern bool __ksu_is_allow_uid_for_current(uid_t uid);
-extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
-			void *envp, int *flags);
-extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr, void *argv,
-				void *envp, int *flags);
-#endif
-
 /*
  * sys_execve() executes a new program.
  */
@@ -1742,20 +1727,6 @@ static int __do_execve_file(int fd, struct filename *filename,
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
-
-#ifdef CONFIG_KSU_SUSFS
-	if (likely(susfs_is_current_proc_umounted()) || !ksu_su_compat_enabled) {
-		goto orig_flow;
-	}
-
-	if (unlikely(ksu_execveat_hook || !susfs_is_boot_completed_triggered)) {
-		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
-	} else if ((__ksu_is_allow_uid_for_current(current_uid().val))) {
-		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
-	}
-
-orig_flow:
-#endif
 
 	/*
 	 * We move the actual failure in case of RLIMIT_NPROC excess from
