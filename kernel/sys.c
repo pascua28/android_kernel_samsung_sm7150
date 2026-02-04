88a4c20af817e20f9bad593657703d4a31b44351
Samuel Pascua <pascua.samuel.14@gmail.com>
2026-02-05 06:52:22 +0800

    kernel: only spoof bpf uname for OneUI
    Change-Id: I9102cf537291da12180fb8fdb84dd75048931cbc
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -73,6 +73,8 @@
 #include <asm/io.h>
 #include <asm/unistd.h>
 
+#include <linux/rom_notifier.h>
+
 #ifndef SET_UNALIGN_CTL
 # define SET_UNALIGN_CTL(a, b)	(-EINVAL)
 #endif
@@ -1207,7 +1209,8 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	    !strncmp(current->comm, "netbpfload", 10) ||
 	    !strncmp(current->comm, "netd", 4) ||
 	    !strncmp(current->comm, "uprobestats", 11)) {
-		if (current_uid().val == 0 && !legacy_ebpf) {
+		if (current_uid().val == 0 && !legacy_ebpf &&
+		    !is_aosp) {
 #if defined(CONFIG_FAKE_UNAME_5_4)
 			strcpy(tmp.release, "5.4.200");
 #elif defined(CONFIG_FAKE_UNAME_5_10)
