5eca9ef7e2d6d52cab055942c55b8cb6400aa726
Samuel Pascua <pascua.samuel.14@gmail.com>
2026-05-01 00:41:56 +0800

    cpuidle: remove qcom cpu isolation
    Change-Id: I9b96d7fc242d64e44d7211b04611f46e1e88f679
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -648,11 +648,8 @@ static void wake_up_idle_cpus(void *v)
 	struct cpumask cpus;
 
 	preempt_disable();
-	if (v) {
-		cpumask_andnot(&cpus, v, cpu_isolated_mask);
+	if (v)
 		cpumask_and(&cpus, &cpus, cpu_online_mask);
-	} else
-		cpumask_andnot(&cpus, cpu_online_mask, cpu_isolated_mask);
 
 	for_each_cpu(cpu, &cpus) {
 		if (cpu == smp_processor_id())
