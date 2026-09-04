6ab5463d1d705d7cef4cec099e11013b3aefda8a
Samuel Pascua <pascua.samuel.14@gmail.com>
2026-09-04 15:09:17 +0800

    android: binder: Fix freecess build after previous commit
    Change-Id: I2733cf54f9722e88b042d39ba8f41151b38b5019
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -6884,6 +6884,7 @@ static void print_binder_proc(struct seq_file *m, struct binder_proc *proc,
 	if (!print_all && m->count == header_pos)
 		m->count = start_pos;
 }
+#endif
 
 #ifdef CONFIG_SAMSUNG_FREECESS
 static void binder_in_transaction(struct binder_proc *proc, int uid)
@@ -7010,6 +7011,7 @@ void binders_in_transcation(int uid)
 }
 #endif
 
+#ifdef CONFIG_ANDROID_BINDER_LOGS
 static const char * const binder_return_strings[] = {
 	"BR_ERROR",
 	"BR_OK",
