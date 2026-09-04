910258382294ee8fabf150c50af2ccd8e40a1bd7
Samuel Pascua <pascua.samuel.14@gmail.com>
2026-09-04 08:12:09 +0800

    Revert "binder: fix trivial kernel-doc typo"
    This reverts commit 4f0a9a4885a11b46249fc92203deb4fb63266eb5.

--- drivers/android/binder_alloc.h
+++ drivers/android/binder_alloc.h
@@ -84,10 +84,10 @@ struct binder_lru_page {
 /**
  * struct binder_alloc - per-binder proc state for binder allocator
  * @vma:                vm_area_struct passed to mmap_handler
- *                      (invariant after mmap)
+ *                      (invarient after mmap)
  * @tsk:                tid for task that called init for this proc
  *                      (invariant after init)
- * @vma_vm_mm:          copy of vma->vm_mm (invariant after mmap)
+ * @vma_vm_mm:          copy of vma->vm_mm (invarient after mmap)
  * @buffer:             base of per-proc address space mapped via mmap
  * @buffers:            list of all buffers for this proc
  * @free_buffers:       rb tree of buffers available for allocation
