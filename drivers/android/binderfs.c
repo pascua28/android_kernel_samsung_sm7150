795ac5951e8b02f0c56ccdc5e52f3ff431f3ecc8
Samuel Pascua <pascua.samuel.14@gmail.com>
2026-09-04 08:12:00 +0800

    Revert "BACKPORT: binderfs: Drop unused #include <linux/radix-tree.h>"
    This reverts commit d1310aacd7cfc72b6150e043de51e11ebd1324c9.

--- drivers/android/binderfs.c
+++ drivers/android/binderfs.c
@@ -19,6 +19,7 @@
 #include <linux/mutex.h>
 #include <linux/mount.h>
 #include <linux/parser.h>
+#include <linux/radix-tree.h>
 #include <linux/sched.h>
 #include <linux/seq_file.h>
 #include <linux/slab.h>
